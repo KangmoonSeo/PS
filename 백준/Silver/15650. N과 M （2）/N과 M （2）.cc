1c3d922e7b76fa59eb97c5dd57d43efaa00df887
Kangmoon Seo <100016044+KangmoonSeo@users.noreply.github.com>
2023-03-11 20:55:25 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
@@ -2,28 +2,27 @@
 using namespace std;
 
 void NandM(int max, int len) {
-  // mem 초기화(1,2,3, ...)
+  // 0. mem 초기화(1,2,3, ...)
   int* mem = new int[len]();
-  for (size_t i = 0; i < len; i++) {
-    mem[i] = 1 + i;
-  }
+  for (int i = 0; i < len; i++) mem[i] = 1 + i;
 
-  while (true) {
+  while (mem[0] < max - len + 2) {
     // 1. 현재 mem 출력
-    for (size_t i = 0; i < len; i++) cout << mem[i] << " ";
+    for (int i = 0; i < len; i++) cout << mem[i] << " ";
     cout << "\n";
 
-    mem[len - 1]++;  // 2. mem 마지막 자리 1 가산
+    // 2. mem 마지막 자리 1 가산
+    mem[len - 1]++;
 
     // 3. 값 조작 및 종결 조건 확인
-    for (size_t i = 0; i < len; i++) {  // 0, 1, ... , len-1
-      int _current = len - i - 1;       // len-1, len-2, ... , 0
+    for (int i = 0; i < len; i++) {
+      int _point = len - i - 1;
 
-      if (mem[_current] > max - i) {  // 현 memory의 허용치(max-i)를 초과하면
-        if (_current == 0) return;  // (종결) mem[0]에서 초과하면 함수 종료
-        mem[_current - 1]++;  // 전단계 메모리 가산
-        for (size_t j = _current; j < len; j++) {
-          mem[j] = mem[j - 1] + 1;  // 전단계 메모리의 값 copy
+      if (mem[_point] > max - i) {  // 현 memory의 허용치(max-i)를 초과하면
+        if (_point == 0) return;  // (종결) mem[0]에서 초과하면 함수 종료
+        mem[_point - 1]++;        // 왼쪽 메모리 가산
+        for (int j = _point; j < len; j++) {
+          mem[j] = mem[j - 1] + 1;  // 왼쪽 메모리 복사
         }
       }
     }
