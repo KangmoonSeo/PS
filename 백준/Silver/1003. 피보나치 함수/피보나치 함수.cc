b05d7c707a3db383fb65337efd99616076e5715e
KangmoonSeo <m_oon17@naver.com>
2023-03-17 13:08:23 +0900

    피보나치 함수 풀이 개선

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.cc
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.cc
@@ -2,7 +2,21 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+// 개선안 1. dp 함수를 0과 1 개별로 받지 않음
+// 개선안 2. for문은 한 번만 사용함. 예외 처리 엄밀하게 준비
 
+// dynamic programming, 실행 속도를 O(n)에 근접하게 맞추기 위함
+int dp[50] = {
+    0,
+    1,
+};
+
+int fibo(int n) {
+  if (n > 1 && dp[n] == 0) {
+    dp[n] = fibo(n - 1) + fibo(n - 2);
+  }
+  return dp[n];
+}
 int main() {
   // 실행속도 최적화 start
   ios::sync_with_stdio(false);
@@ -11,27 +25,18 @@ int main() {
   // 실행속도 최적화 end
 
   int n;
-
   cin >> n;
 
   // input
-  vector<int> input(n);
-  int max = 0;
-  for (size_t i = 0; i < n; i++) {
-    cin >> input[i];
-    max = max < input[i] ? input[i] : max;
-  }
-  // dynamic programming
-  vector<int> dp_0(max + 1, 0);
-  vector<int> dp_1(max + 1, 0);
-  dp_0[0] = 1, dp_1[1] = 1;
+  int input;
 
-  for (size_t i = 2; i <= max; i++) {
-    dp_0[i] = dp_0[i - 1] + dp_0[i - 2];
-    dp_1[i] = dp_1[i - 1] + dp_1[i - 2];
-  }
-  // output
   for (size_t i = 0; i < n; i++) {
-    cout << dp_0[input[i]] << " " << dp_1[input[i]] << "\n";
+    cin >> input;
+    // output
+    if (input == 0) {
+      cout << "1 0\n";
+    } else {
+      cout << fibo(input - 1) << " " << fibo(input) << "\n";
+    }
   }
 }
