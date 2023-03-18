e9fdea5b9ea163a72b1e77d718c6f21196f0cf56
Kangmoon Seo <100016044+KangmoonSeo@users.noreply.github.com>
2023-03-18 18:10:06 +0900

    [Gold IV] Title: 테트로미노, Time: 16 ms, Memory: 2944 KB -BaekjoonHub

--- 백준/Gold/14500. 테트로미노/테트로미노.cc
+++ 백준/Gold/14500. 테트로미노/테트로미노.cc
@@ -1,5 +1,4 @@
 #include <algorithm>
-#include <cmath>
 #include <iostream>
 #include <vector>
 using namespace std;
@@ -14,7 +13,7 @@ int main() {
   int n, m;  // input
   cin >> n >> m;
 
-  vector<vector<int>> ar(n + 6, vector<int>(m + 6));  // output
+  vector<vector<int>> ar(n + 6, vector<int>(m + 6));
 
   for (size_t i = 3; i < n + 3; i++) {
     for (size_t j = 3; j < m + 3; j++) {
@@ -25,17 +24,17 @@ int main() {
   // brute force algorithm, (i,j)->(0->n,0->m) 모든 케이스 시행 후 최대값 출력
   for (size_t i = 3; i < n + 3; i++) {
     for (size_t j = 3; j < m + 3; j++) {
-      // 4x1
+      // 1. I 블럭
       rs = max(rs, (ar[i][j] + ar[i + 1][j] + ar[i + 2][j] + ar[i + 3][j]));
       rs = max(rs, (ar[i][j] + ar[i][j + 1] + ar[i][j + 2] + ar[i][j + 3]));
-      // 2x2
+      // 2. O 블럭
       rs = max(rs, (ar[i][j] + ar[i + 1][j] + ar[i][j + 1] + ar[i + 1][j + 1]));
-      // ㅏ
+      // 3. T 블럭
       rs = max(rs, (ar[i][j] + ar[i + 1][j] + ar[i + 2][j] + ar[i + 1][j + 1]));
       rs = max(rs, (ar[i][j] + ar[i + 1][j] + ar[i + 2][j] + ar[i + 1][j - 1]));
       rs = max(rs, (ar[i][j] + ar[i][j + 1] + ar[i][j + 2] + ar[i + 1][j + 1]));
       rs = max(rs, (ar[i][j] + ar[i][j + 1] + ar[i][j + 2] + ar[i - 1][j + 1]));
-      // '--,
+      // 4. S,Z 블럭
       rs = max(rs,
                (ar[i][j] + ar[i + 1][j] + ar[i + 1][j + 1] + ar[i + 2][j + 1]));
       rs = max(rs,
@@ -44,7 +43,7 @@ int main() {
                (ar[i][j] + ar[i][j + 1] + ar[i + 1][j + 1] + ar[i + 1][j + 2]));
       rs = max(rs,
                (ar[i][j] + ar[i][j + 1] + ar[i - 1][j + 1] + ar[i - 1][j + 2]));
-      // ㄴ
+      // 5. L,J 블럭
       rs = max(rs, (ar[i][j] + ar[i + 1][j] + ar[i + 2][j] + ar[i + 2][j + 1]));
       rs = max(rs, (ar[i][j] + ar[i + 1][j] + ar[i + 2][j] + ar[i + 2][j - 1]));
       rs = max(rs, (ar[i][j] + ar[i + 1][j] + ar[i + 2][j] + ar[i][j + 1]));
@@ -55,48 +54,5 @@ int main() {
       rs = max(rs, (ar[i][j] + ar[i][j + 1] + ar[i][j + 2] + ar[i - 1][j]));
     }
   }
-  cout << rs << endl;
+  cout << rs << endl;  // output
 }
-
-/*
-
-      rs = max({rs, (ar[i][j] + ar[i + 1][j] + ar[i + 2][j] +
-                             ar[i + 3][j])});
-      // (0,0 1,0 2,0 2,1) L
-      rs = max({rs, (ar[i][j] + ar[i + 1][j] + ar[i + 2][j] +
-                             ar[i + 2][j + 1])});
-      // (0,0 1,0 1,1 2,0) ㅏ
-      rs = max({rs, (ar[i][j] + ar[i + 1][j] + ar[i + 1][j + 1] +
-                             ar[i + 2][j])});
-      // (0,0 1,0 1,1 2,1)
-      rs = max({rs, (ar[i][j] + ar[i + 1][j] + ar[i + 1][j + 1] +
-                             ar[i + 2][j + 1])});
-      // (0,0 1,0 1,1 1,2) ㄴ
-      rs = max({rs, (ar[i][j] + ar[i + 1][j] + ar[i + 1][j + 1] +
-                             ar[i + 1][j + 2])});
-
-      // 2. (0,1) max -> (0,0 0,1 1,1 1,0),
-      rs = max({rs, (ar[i][j] + ar[i][j + 1] + ar[i + 1][j + 1] +
-                             ar[i + 1][j])});
-      // (0,0 0,1 1,1 2,1),
-      rs = max({rs, (ar[i][j] + ar[i][j + 1] + ar[i + 1][j + 1] +
-                             ar[i + 2][j + 1])});
-      // (0,0 0,1 1,1 1,2)
-      rs = max({rs, (ar[i][j] + ar[i][j + 1] + ar[i + 1][j + 1] +
-                             ar[i + 1][j + 2])});
-
-      // 3. (0,2) max -> (0,0 0,1 0,2 1,2)
-      rs = max({rs, (ar[i][j] + ar[i][j + 1] + ar[i][j + 2] +
-                             ar[i + 1][j + 2])});
-      // (0,0 0,1 0,2 1,1)
-      rs = max({rs, (ar[i][j] + ar[i][j + 1] + ar[i][j + 2] +
-                             ar[i + 1][j + 1])});
-      // (0,0 0,1 0,2 1,0)
-      rs = max({rs, (ar[i][j] + ar[i][j + 1] + ar[i][j + 2] +
-                             ar[i + 1][j])});
-
-      // 4. (0,3) max -> (0,0 0,1 0,2 0,3)
-      rs = max({rs, (ar[i][j] + ar[i][j + 1] + ar[i][j + 2] +
-                             ar[i][j + 3])});
-
-*/
