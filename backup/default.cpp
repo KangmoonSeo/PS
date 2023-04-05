770b26556f56d8b412e331bd3aeec096ec4320d5
KangmoonSeo <m_oon17@naver.com>
2023-04-05 19:27:43 +0900

    default.cpp 코드 백업

---
+++ backup/default.cpp
@@ -0,0 +1,21 @@
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void func(int n) {
+  // function acts here
+  //
+}
+
+int main() {
+  // 실행속도 최적화 start
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  cout.tie(0);
+  // 실행속도 최적화 end
+
+  int n;  // input
+  cin >> n;
+}
