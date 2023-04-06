fa8b123d9d618abfe883d306592046c890bf16a0
Kangmoon Seo <100016044+KangmoonSeo@users.noreply.github.com>
2023-04-06 19:29:40 +0900

    [Silver I] Title: IOIOI, Time: 0 ms, Memory: 2168 KB -BaekjoonHub

---
+++ 백준/Silver/5525. IOIOI/IOIOI.cc
@@ -0,0 +1,41 @@
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void func(int n, int m, string s) {
+  string target = "IOI";
+  string tmp = "" + s;
+
+  for (int i = 1; i < n; i++) {
+    target += "OI";
+  }
+  int idx = -1;
+  int mem = 0;
+
+  int cnt = 0;
+  for (int i = 0; i < m; i++) {
+    idx = tmp.find(target);
+    if (idx == -1) {
+      break;
+    }
+    tmp = tmp.substr(idx + 2);
+    cnt++;
+  }
+  cout << cnt;
+}
+
+int main() {
+  // 실행속도 최적화 start
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  cout.tie(0);
+  // 실행속도 최적화 end
+
+  int n, m;  // input
+  cin >> n >> m;
+  string s;
+  cin >> s;
+  func(n, m, s);
+}
