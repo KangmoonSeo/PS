4e85d7ac1efccfe95ad956ffb3101924bd8a6e97
Kangmoon Seo <100016044+KangmoonSeo@users.noreply.github.com>
2023-04-09 15:18:21 +0900

    [Silver V] Title: 집합, Time: 744 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.cc
+++ 백준/Silver/11723. 집합/집합.cc
@@ -4,46 +4,46 @@
 #include <vector>
 using namespace std;
 
-/*
- * add x: S에 x를 추가한다. (1 ≤ x ≤ 20) S에 x가 이미 있는 경우에는 연산을
- * 무시한다.
- * remove x: S에서 x를 제거한다. (1 ≤ x ≤ 20) S에 x가 없는 경우에는 연산을
- * 무시한다.
- * check x: S에 x가 있으면 1을, 없으면 0을 출력한다. (1 ≤ x ≤ 20)
- * toggle x: S에 x가 있으면 x를 제거하고, 없으면 x를 추가한다. (1 ≤ x ≤ 20)
- * all: S를 {1, 2, ..., 20} 으로 바꾼다.
- * empty: S를 공집합으로 바꾼다.
- */
-void func(int n) {
-  vector<int> arr(21);
+void set(int n) {
   vector<int> _all(21);
-  vector<int> _empty(21);
   for (int i = 0; i < 21; i++) _all[i] = i;
+  vector<int> _empty(21);
+
+  vector<int> arr(21);
 
   string command;
   int input;
 
   for (int i = 0; i < n; i++) {
     cin >> command;
+
     if (command == "all") {
       arr.assign(_all.begin(), _all.end());
-
-    } else if (command == "empty") {
+      continue;
+    }
+    if (command == "empty") {
       arr.assign(_empty.begin(), _empty.end());
-
-    } else if (command == "add") {
+      continue;
+    }
+    if (command == "add") {
       cin >> input;
       arr[input] = input;
-    } else if (command == "remove") {
+      continue;
+    }
+    if (command == "remove") {
       cin >> input;
       arr[input] = 0;
-    } else if (command == "check") {
+      continue;
+    }
+    if (command == "check") {
       cin >> input;
       cout << ((arr[input] == input) ? 1 : 0) << "\n";
-
-    } else if (command == "toggle") {
+      continue;
+    }
+    if (command == "toggle") {
       cin >> input;
-      arr[input] = arr[input] == 0 ? input : 0;
+      arr[input] = ((arr[input] == 0) ? input : 0);
+      continue;
     }
   }
 }
@@ -57,5 +57,5 @@ int main() {
 
   int n;  // input
   cin >> n;
-  func(n);
+  set(n);
 }
