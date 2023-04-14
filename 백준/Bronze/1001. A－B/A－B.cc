5121dce7b2409db8b1943d7e7cc991589f807a02
Kangmoon Seo <100016044+KangmoonSeo@users.noreply.github.com>
2023-04-14 23:59:29 +0900

    [Bronze V] Title: A-B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.cc
@@ -0,0 +1,17 @@
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+  // 실행속도 최적화 start
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  cout.tie(0);
+  // 실행속도 최적화 end
+
+  int n1, n2;  // input
+  cin >> n1 >> n2;
+  cout << n1 - n2;
+}
