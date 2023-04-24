e2dab0f9e4aa3ea799fe0f69d41c90f12f86830b
Kangmoon Seo <100016044+KangmoonSeo@users.noreply.github.com>
2023-04-24 20:42:28 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 156 ms, Memory: 20868 KB -BaekjoonHub

---
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.cc
@@ -0,0 +1,39 @@
+#include <algorithm>
+#include <cctype>
+#include <cmath>
+#include <iostream>
+#include <map>
+#include <string>
+#include <utility>
+#include <vector>
+using namespace std;
+
+int main() {
+  // 실행속도 최적화 start
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  cout.tie(0);
+  // 실행속도 최적화 end
+
+  int n, m;  // input
+  cin >> n >> m;
+  string s_input;
+
+  map<string, int> i_data;
+  vector<string> s_data(n + 1);
+
+  for (int i = 1; i <= n; i++) {
+    cin >> s_input;
+    i_data[s_input] = i;
+    s_data[i] = s_input;
+  } 
+
+  for (int j = 0; j < m; j++) {
+    cin >> s_input;
+    if (!isdigit(s_input[0])) {  // string
+      cout << i_data[s_input] << "\n";
+    } else {  // int
+      cout << s_data[stoi(s_input)] << "\n";
+    }
+  }
+}
