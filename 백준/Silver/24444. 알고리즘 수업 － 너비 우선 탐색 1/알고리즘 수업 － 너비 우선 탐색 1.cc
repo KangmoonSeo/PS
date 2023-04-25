941b1fde2d0d93835f22c6d46a1cdbe05b483e26
Kangmoon Seo <100016044+KangmoonSeo@users.noreply.github.com>
2023-04-25 21:52:45 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 96 ms, Memory: 10432 KB -BaekjoonHub

--- 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.cc
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.cc
@@ -1,14 +1,14 @@
 #include <algorithm>
 #include <cmath>
 #include <iostream>
-#include <map>
 #include <queue>
 #include <string>
 #include <utility>
 #include <vector>
 using namespace std;
+typedef priority_queue<int, vector<int>, greater<int> > PQ;
 bool isVisited[100001] = {false};
-vector<priority_queue<int, vector<int>, greater<int> > > adj(100001);
+vector<PQ> adj(100001);
 int arrCnt[100001] = {0};  // 문제 조건
 
 void add(int a, int b) {
@@ -16,31 +16,25 @@ void add(int a, int b) {
   adj[b].push(a);
 }
 
-void bfs(int r, int n) {
+void bfs(int r) {
   queue<int> q;
   q.push(r);
   isVisited[r] = true;
-  int cnt = 0;
+  int cnt = 0;  // 문제 조건, BFS 실행 순서 추적
 
   while (!q.empty()) {
     int x = q.front();
     q.pop();
-    // 문제 조건, BFS 실행 순서 담기
-    arrCnt[x] = ++cnt;
+    arrCnt[x] = ++cnt;  // BFS 실행 순서 담음
 
     int length = adj[x].size();
     while (length--) {
-      if (isVisited[adj[x].top()]) {
-        adj[x].pop();
-        continue;
+      if (!isVisited[adj[x].top()]) {
+        q.push(adj[x].top());
+        isVisited[adj[x].top()] = true;
       }
-      q.push(adj[x].top());
-      isVisited[adj[x].top()] = true;
       adj[x].pop();
     }
-  }
-  for (int i = 1; i <= n; i++) {
-    cout << arrCnt[i] << "\n";
   }
 }
 
@@ -53,10 +47,14 @@ int main() {
 
   int n, m, r;  // input
   cin >> n >> m >> r;
+
   int i1, i2;
   for (int i = 0; i < m; i++) {
     cin >> i1 >> i2;
     add(i1, i2);
   }
-  bfs(r, n);
+  bfs(r);
+  for (int i = 1; i <= n; i++) {
+    cout << arrCnt[i] << "\n";
+  }
 }
