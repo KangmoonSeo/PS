acb0738bc181cbed673ed5cbbddd7f441cf95d07
Kangmoon Seo <100016044+KangmoonSeo@users.noreply.github.com>
2023-05-01 17:36:45 +0900

    [Silver I] Title: 트리 순회, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/1991. 트리 순회/트리 순회.cc
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+map<char, pair<char, char> > arr;
+
+void traverse(char ch, int mode) {  // mode:0, 1, 2
+  if (ch == '.') return;
+  if (mode == 0) cout << ch;
+  traverse(arr[ch].first, mode);
+  if (mode == 1) cout << ch;
+  traverse(arr[ch].second, mode);
+  if (mode == 2) cout << ch;
+}
+void solve(int n = 0) {
+  traverse('A', 0);
+  cout << "\n";
+  traverse('A', 1);
+  cout << "\n";
+  traverse('A', 2);
+  cout << "\n";
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  cout.tie(0);  // optimization
+
+  int n;  // input
+  cin >> n;
+  char c1, c2, c3;
+  while (n--) {
+    cin >> c1 >> c2 >> c3;
+    arr[c1].first = c2;
+    arr[c1].second = c3;
+  }
+  solve();
+}
