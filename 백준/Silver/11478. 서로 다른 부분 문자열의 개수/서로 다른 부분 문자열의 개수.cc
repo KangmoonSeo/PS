e60419fc00b355aab46ad9c47a9c5305dfb33906
Kangmoon Seo <100016044+KangmoonSeo@users.noreply.github.com>
2023-05-06 18:50:05 +0900

    [Silver III] Title: 서로 다른 부분 문자열의 개수, Time: 928 ms, Memory: 212040 KB -BaekjoonHub

---
+++ 백준/Silver/11478. 서로 다른 부분 문자열의 개수/서로 다른 부분 문자열의 개수.cc
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(string const s) {
+  map<string, bool> m;
+  int length = s.size();
+  string tmp;
+  for (int l = 1; l <= length; l++) {
+    for (int i = 0; i <= length - l; i++) {
+      tmp.assign(s.begin() + i, s.begin() + i + l);
+      m[tmp] = true;
+    }
+  }
+  cout << m.size() << "\n";
+}
+
+int main() {
+  ios::sync_with_stdio(false), cin.tie(0), cout.tie(0);  // optimization
+
+  string s;  // input
+  cin >> s;
+  solve(s);
+}
