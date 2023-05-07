dd191af2636878c82c11da5d3baa68bedefc109c
KangmoonSeo <m_oon17@naver.com>
2023-05-07 19:13:30 +0900

    fix: shell이 폴더 안에서만 작동하도록 path 추가

--- backup/remove.sh
+++ backup/remove.sh
@@ -1,6 +1,10 @@
 ## default.cpp, .vscode 구성요소, 자기 자신을 제외하고
 ## 실행 파일, 폴더 내 7일이 지난 파일을 삭제하는 script
 
-find . -mtime +7 -not -path "./vscode" -not -path "./.vscode/*" -not -path "./remove.sh" -not -path "./default.cpp" -delete
-find . -type f -not -name "*.*" -delete
-find . -name 'tempCodeRunner*' -delete
+## config
+__PATH="/Users/User/Library/CloudStorage/OneDrive-인하대학교/git/PS/testplace"
+
+## deletion
+find ${__PATH} -mtime +7 -not -path "./vscode" -not -path "./.vscode/*" -not -path "./remove.sh" -not -path "./default.cpp" -delete
+find ${__PATH} -type f -not -name "*.*" -delete
+find ${__PATH} -name 'tempCodeRunner*' -delete
