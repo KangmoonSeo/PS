aee5c375ca5c874e95ef733a4ded334f823e9964
Kangmoon Seo <100016044+KangmoonSeo@users.noreply.github.com>
2023-05-07 16:44:43 +0900

    [Silver I] Title: 스티커, Time: 80 ms, Memory: 2680 KB -BaekjoonHub

--- 백준/Silver/9465. 스티커/스티커.cc
+++ 백준/Silver/9465. 스티커/스티커.cc
@@ -6,8 +6,7 @@ void sticker() {
   int n;
   cin >> n;
   int arr[100001][2];
-  int dp[100001][2];
-  
+
   for (int i = 0; i < 2; i++) {
     for (int j = 1; j <= n; j++) {
       cin >> arr[j][i];
@@ -15,10 +14,10 @@ void sticker() {
   }
   for (int j = 1; j <= n; j++) {
     for (int i = 0; i < 2; i++) {
-      dp[j][i] = max(arr[j][i] + dp[j - 1][1 - i], dp[j - 1][i]);
+      arr[j][i] = max(arr[j][i] + arr[j - 1][1 - i], arr[j - 1][i]);
     }
   }
-  int ans = max(dp[n][0], dp[n][1]);
+  int ans = max(arr[n][0], arr[n][1]);
   cout << ans << "\n";
 }
 
