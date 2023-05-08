ac0177b9096be28fac3585c9a7131b0caf56f02d
Kangmoon Seo <100016044+KangmoonSeo@users.noreply.github.com>
2023-05-08 19:19:32 +0900

    [Silver III] Title: N과 M (4), Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.cc
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.cc
@@ -2,38 +2,34 @@
 using namespace std;
 
 int n, m;  // input
+vector<int> arr;
 
-void recur(vector<int> st, int TTL) {
-  vector<int> tmp = st;
-
-  for (int i = st.back(); i <= n; i++) {
-    if (TTL == 0) {
-      for (int j = 0; j < tmp.size(); j++) {
-        cout << tmp[j] << " ";
-      }
-      cout << i << "\n";
-    } else {
-      tmp.push_back(i);
-      recur(tmp, TTL - 1);
-      tmp.pop_back();
+void recur(int TTL) {
+  if (TTL == 0) {
+    int length = arr.size();
+    for (int i = 0; i < length; i++) {
+      cout << arr[i] << " ";
     }
+    cout << "\n";
+    return;
+  }
+
+  int b = arr.back();
+  for (int i = b; i <= n; i++) {
+    arr.push_back(i);
+    recur(TTL - 1);
+    arr.pop_back();
   }
 }
+
 int main() {
   ios::sync_with_stdio(false), cin.tie(0), cout.tie(0);  // optimization
 
   cin >> n >> m;
-  vector<int> tmp;
-  if (m < 2) {
-    for (int i = 1; i <= n; i++) {
-      cout << i << "\n";
-    }
-    return 0;
-  }
 
   for (int i = 1; i <= n; i++) {
-    tmp.clear();
-    tmp.push_back(i);
-    recur(tmp, m - 2);
+    arr.push_back(i);
+    recur(m - 1);
+    arr.pop_back();
   }
 }
