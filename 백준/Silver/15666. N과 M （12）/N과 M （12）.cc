1c5a366a13e2799bac2aeafd89bde69f062d122e
Kangmoon Seo <100016044+KangmoonSeo@users.noreply.github.com>
2023-05-09 15:15:23 +0900

    [Silver II] Title: N과 M (12), Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.cc
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n, m;
+vector<int> field;
+vector<int> bt;
+map<int, int> bb;
+void BT(int init = 0) {
+  if (init == m) {
+    int length = bt.size();
+    for (int i = 0; i < length; i++) {
+      cout << bt[i] << " ";
+    }
+    cout << "\n";
+    return;
+  }
+  int b;
+  if (bt.empty()) {
+    b = 0;
+  } else {
+    b = bb[bt.back()];
+  }
+  for (int i = b; i < n; i++) {
+    bt.push_back(field[i]);
+    BT(init + 1);
+    bt.pop_back();
+  }
+}
+
+int main() {
+  ios::sync_with_stdio(false), cin.tie(0), cout.tie(0);  // optimization
+
+  cin >> n >> m;
+  int input;
+  for (int i = 0; i < n; i++) {
+    cin >> input;
+    field.push_back(input);
+  }
+  sort(field.begin(), field.end());
+  field.erase(unique(field.begin(), field.end()), field.end());
+  n = field.size();
+  for (int i = 0; i < n; i++) {
+    bb[field[i]] = i;
+  }
+
+  BT();
+}
