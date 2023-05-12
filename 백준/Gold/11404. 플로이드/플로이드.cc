a5af2d0189ef2da696bb41a3dac30e5c9437f170
Kangmoon Seo <100016044+KangmoonSeo@users.noreply.github.com>
2023-05-12 16:35:44 +0900

    [Gold IV] Title: 플로이드, Time: 32 ms, Memory: 2060 KB -BaekjoonHub

---
+++ 백준/Gold/11404. 플로이드/플로이드.cc
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define MAX_ 1e9
+#define min(a, b) a > b ? b : a;
+
+int D[101][101];
+int n;
+
+void add(int a, int b, int n = 1) {
+  if (D[a][b] == 0) D[a][b] = n;
+  D[a][b] = min(D[a][b], n);
+}
+
+void print() {
+  for (int i = 1; i <= n; i++) {
+    for (int j = 1; j <= n; j++) {
+      if (D[i][j] == MAX_)
+        cout << "0 ";
+      else
+        cout << D[i][j] << " ";
+    }
+    cout << "\n";
+  }
+}
+
+void floid() {
+  for (int k = 1; k <= n; k++) {
+    for (int i = 1; i <= n; i++) {
+      for (int j = 1; j <= n; j++) {
+        if (i == j) continue;
+        D[i][j] = min(D[i][j], D[i][k] + D[k][j]);  // floid
+      }
+    }
+  }
+}
+
+void solve() {
+  floid();
+  print();
+}
+
+int main() {
+  ios::sync_with_stdio(false), cin.tie(0), cout.tie(0);  // optimization
+
+  int m, i1, i2, i3;
+  cin >> n >> m;
+
+  fill(&D[0][0], &D[100][101], MAX_);
+
+  while (m--) {
+    cin >> i1 >> i2 >> i3;
+    add(i1, i2, i3);
+  }
+  solve();
+}
