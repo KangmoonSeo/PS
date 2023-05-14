52a1a18f62fa6308b13a8584975da08babc9c63e
KangmoonSeo <m_oon17@naver.com>
2023-05-14 23:11:13 +0900

    update: default.cpp 최신화

--- backup/default.cpp
+++ backup/default.cpp
@@ -1,7 +1,10 @@
 #include <bits/stdc++.h>
+#define max(a, b) a > b ? a : b
+#define min(a, b) a > b ? b : a
 using namespace std;
+int n;
 
-void solve(int n = 0) {
+void solve() {
   // function acts here
   //
 }
@@ -9,6 +12,6 @@ void solve(int n = 0) {
 int main() {
   ios::sync_with_stdio(false), cin.tie(0), cout.tie(0);  // optimization
 
-  int n;  // input
   cin >> n;
+  solve();
 }
