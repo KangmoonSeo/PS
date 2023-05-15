af99e3bf8da46d74eb973e37941d98b087cfe34a
Kangmoon Seo <100016044+KangmoonSeo@users.noreply.github.com>
2023-05-15 21:37:38 +0900

    [Silver I] Title: 1로 만들기 2, Time: 4 ms, Memory: 4716 KB -BaekjoonHub

---
+++ 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.cc
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+#define max(a, b) a > b ? a : b
+#define min(a, b) a > b ? b : a
+using namespace std;
+int n;
+bool visited[1000001] = {};
+void solve() {
+  queue<pair<int, vector<int> > > q;
+  vector<int> init_vec;
+  init_vec.push_back(n);
+  q.push(make_pair(n, init_vec));
+  visited[n] = true;
+  while (!q.empty()) {
+    int val = q.front().first;
+    vector<int> vec = q.front().second;
+    visited[val] = true;
+    q.pop();
+    if (val == 1) {
+      cout << vec.size() - 1 << "\n";
+      for (int i = 0; i < vec.size(); i++) {
+        cout << vec[i] << " ";
+      }
+      cout << "\n";
+      return;
+    }
+
+    if (val % 3 == 0 && !visited[val / 3]) {
+      vec.push_back(val / 3);
+      q.push(make_pair(val / 3, vec));
+      vec.pop_back();
+    }
+    if (val % 2 == 0 && !visited[val / 2]) {
+      vec.push_back(val / 2);
+      q.push(make_pair(val / 2, vec));
+      vec.pop_back();
+    }
+    if (!visited[val - 1]) {
+      vec.push_back(val - 1);
+      q.push(make_pair(val - 1, vec));
+      vec.pop_back();
+    }
+  }
+}
+
+int main() {
+  ios::sync_with_stdio(false), cin.tie(0), cout.tie(0);  // optimization
+
+  cin >> n;
+  solve();
+}
