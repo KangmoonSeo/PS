3d34a8a365cb43d3491f864bbe75eeb55babe60c
Kangmoon Seo <100016044+KangmoonSeo@users.noreply.github.com>
2023-05-15 21:31:55 +0900

    [Silver III] Title: 1로 만들기, Time: 0 ms, Memory: 3128 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.cc
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -1,26 +1,31 @@
-#include <algorithm>
-#include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
+#define max(a, b) a > b ? a : b
+#define min(a, b) a > b ? b : a
 using namespace std;
+int n;
+bool visited[1000001] = {};
+void solve() {
+  queue<pair<int, int> > q;
+  q.push(make_pair(n, 0));
+  visited[n] = true;
+  while (!q.empty()) {
+        int val = q.front().first;
+    int cnt = q.front().second;
+    visited[val] = true;
+    q.pop();
+    if (val == 1) {
+      cout << cnt << "\n";
+      return;
+    }
+    if (val % 3 == 0 && !visited[val / 3]) q.push(make_pair(val / 3, cnt + 1));
+    if (val % 2 == 0 && !visited[val / 2]) q.push(make_pair(val / 2, cnt + 1));
+    if (!visited[val - 1]) q.push(make_pair(val - 1, cnt + 1));
+  }
+}
 
 int main() {
-  // 실행속도 최적화 start
-  ios::sync_with_stdio(false);
-  cin.tie(0);
-  cout.tie(0);
-  // 실행속도 최적화 end
-
-  int n;  // input
+  ios::sync_with_stdio(false), cin.tie(0), cout.tie(0);  // optimization
 
   cin >> n;
-  vector<int> dp(n + 1,
-                 0);  // sol) 시간을 최소화하기 위한(O(n)) dynamic programming,
-                      // bottom-up 방식으로 배열을 만들어 중복을 피한다.
-
-  for (size_t i = 2; i <= n; i++) {
-    dp[i] = dp[i - 1] + 1;
-    if (!(i % 3)) dp[i] = min({dp[i], dp[i / 3] + 1});
-    if (!(i % 2)) dp[i] = min({dp[i], dp[i / 2] + 1});
-  }
-  cout << dp[n];
+  solve();
 }
