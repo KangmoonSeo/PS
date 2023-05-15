749334d8bb349bd1d9c8629312086df0a3754377
Kangmoon Seo <100016044+KangmoonSeo@users.noreply.github.com>
2023-05-15 21:44:01 +0900

    [Silver I] Title: 1로 만들기 2, Time: 0 ms, Memory: 3132 KB -BaekjoonHub

--- 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.cc
+++ 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.cc
@@ -4,37 +4,41 @@
 using namespace std;
 int n;
 bool visited[1000001] = {};
+
 void solve() {
   queue<pair<int, vector<int> > > q;
-  vector<int> init_vec;
-  init_vec.push_back(n);
-  q.push(make_pair(n, init_vec));
+  q.push(make_pair(n, vector<int>(1, n)));
   visited[n] = true;
+
   while (!q.empty()) {
     int val = q.front().first;
     vector<int> vec = q.front().second;
-    visited[val] = true;
     q.pop();
+
     if (val == 1) {
-      cout << vec.size() - 1 << "\n";
-      for (int i = 0; i < vec.size(); i++) {
+      int len = vec.size();
+      cout << len - 1 << "\n";
+      for (int i = 0; i < len; i++) {
         cout << vec[i] << " ";
       }
       cout << "\n";
-      return;
+      break;
     }
 
     if (val % 3 == 0 && !visited[val / 3]) {
+      visited[val / 3] = true;
       vec.push_back(val / 3);
       q.push(make_pair(val / 3, vec));
       vec.pop_back();
     }
     if (val % 2 == 0 && !visited[val / 2]) {
+      visited[val / 2] = true;
       vec.push_back(val / 2);
       q.push(make_pair(val / 2, vec));
       vec.pop_back();
     }
     if (!visited[val - 1]) {
+      visited[val - 1] = true;
       vec.push_back(val - 1);
       q.push(make_pair(val - 1, vec));
       vec.pop_back();
