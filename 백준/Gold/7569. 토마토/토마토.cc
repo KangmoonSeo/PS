25186c32ecf76f4e9b76c48dcefe15bb2914f205
Kangmoon Seo <100016044+KangmoonSeo@users.noreply.github.com>
2023-05-16 17:35:13 +0900

    [Gold V] Title: 토마토, Time: 96 ms, Memory: 8096 KB -BaekjoonHub

---
+++ 백준/Gold/7569. 토마토/토마토.cc
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int dz[6] = {1, 0, 0, -1, 0, 0};
+const int dy[6] = {0, 1, 0, 0, -1, 0};
+const int dx[6] = {0, 0, 1, 0, 0, -1};
+int z, y, x;
+vector<vector<vector<int> > > arr(102,
+                                  vector<vector<int> >(102,
+                                                       vector<int>(102, -1)));
+
+void tomato() {
+  queue<tuple<int, int, int> > q;
+  int ans = 1;
+  // input
+  for (int k = 1; k <= z; k++) {
+    for (int j = 1; j <= y; j++) {
+      for (int i = 1; i <= x; i++) {
+        cin >> arr[k][j][i];
+        if (arr[k][j][i] == 1) {
+          q.push({k, j, i});
+        }
+      }
+    }
+  }
+  // bfs
+  while (!q.empty()) {
+    tuple<int, int, int> p = q.front();
+    int pz = get<0>(p);
+    int py = get<1>(p);
+    int px = get<2>(p);
+    ans = arr[pz][py][px];
+    q.pop();
+    for (int d = 0; d < 6; d++) {
+      if (arr[pz + dz[d]][py + dy[d]][px + dx[d]] == 0) {
+        arr[pz + dz[d]][py + dy[d]][px + dx[d]] = ans + 1;
+        q.push({pz + dz[d], py + dy[d], px + dx[d]});
+      }
+    }
+  }
+  // exception
+  for (int k = 1; k <= z; k++) {
+    for (int j = 1; j <= y; j++) {
+      for (int i = 1; i <= x; i++) {
+        if (arr[k][j][i] == 0) {
+          cout << "-1\n";
+          return;
+        }
+      }
+    }
+  }
+  // ans
+  cout << ans - 1 << "\n";
+}
+
+int main() {
+  ios::sync_with_stdio(false), cin.tie(0), cout.tie(0);  // optimization
+
+  cin >> x >> y >> z;
+  tomato();
+}
