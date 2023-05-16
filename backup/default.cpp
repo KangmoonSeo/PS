78334afdad6eca57d68fbed2fb03615c890bf46c
KangmoonSeo <m_oon17@naver.com>
2023-05-16 16:09:24 +0900

    update: remove.sh가 ~/.zsh_history를 청소하도록 수정

--- backup/default.cpp
+++ backup/default.cpp
@@ -1,7 +1,6 @@
 #include <bits/stdc++.h>
-#define max(a, b) a > b ? a : b
-#define min(a, b) a > b ? b : a
 using namespace std;
+
 int n;
 
 void solve() {
--- backup/remove.sh
+++ backup/remove.sh
@@ -2,9 +2,13 @@
 ## 실행 파일, 폴더 내 7일이 지난 파일을 삭제하는 script
 
 ## config
-__PATH="/Users/User/Library/CloudStorage/OneDrive-인하대학교/git/PS/testplace"
+__PATH="~/PS/testplace"
 
 ## deletion
-find ${__PATH} -mtime +7 -not -path "./vscode" -not -path "./.vscode/*" -not -path "./remove.sh" -not -path "./default.cpp" -delete
+find ${__PATH} -mtime +7 -not -path "${__PATH}/remove.sh" -not -path "${__PATH}/default.cpp" -delete
 find ${__PATH} -type f -not -name "*.*" -delete
 find ${__PATH} -name 'tempCodeRunner*' -delete
+
+## .zsh_history optimization
+grep -v 'OneDrive' ~/.zsh_history > ~/.zsh_history_temp
+mv ~/.zsh_history_temp ~/.zsh_history
