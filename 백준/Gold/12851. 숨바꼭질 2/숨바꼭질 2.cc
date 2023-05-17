7160ead401c8c4c5e8a9a5c14594287c99403543
Kangmoon Seo <100016044+KangmoonSeo@users.noreply.github.com>
2023-05-17 21:13:27 +0900

    [Gold IV] Title: 숨바꼭질 2, Time: 104 ms, Memory: 10788 KB -BaekjoonHub

---
+++ 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.cc
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, k;
+
+void solve() {
+  // bool visited[200003] = {};
+  map<int, bool> visited;
+  queue<pair<int, int> > q;
+  int ans = 0;
+  q.push(make_pair(n, 0));
+  visited[n] = true;
+  int endTime = 100001;
+  while (!q.empty()) {
+    int curX = q.front().first;
+    int t = q.front().second;
+    visited[curX] = true;
+    q.pop();
+
+    if (endTime < t) {
+      break;
+    } else if (curX == k) {
+      endTime = t;
+      ans++;
+    } else {
+      if (!visited[curX + 1] && curX < k) {
+        q.push(make_pair(curX + 1, t + 1));
+      }
+      if (!visited[curX * 2] && curX < k) {
+        q.push(make_pair(curX * 2, t + 1));
+      }
+      if (!visited[curX - 1]) {
+        q.push(make_pair(curX - 1, t + 1));
+      }
+    }
+  }
+
+  cout << endTime << "\n" << ans << "\n";
+}
+
+int main() {
+  ios::sync_with_stdio(false), cin.tie(0), cout.tie(0);  // optimization
+
+  cin >> n >> k;
+  solve();
+}
