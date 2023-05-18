34da05201530a3db29fbfe18e63970216a0ea43f
Kangmoon Seo <100016044+KangmoonSeo@users.noreply.github.com>
2023-05-18 15:49:54 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 52 ms, Memory: 8060 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.cc
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.cc
@@ -5,30 +5,32 @@ int n;
 vector<vector<int> > adj(100001, vector<int>());
 vector<int> parent(100001, 0);
 
-void dfs(int v) {
-  if (parent[v] == 0) return;
-  vector<int> vec = adj[v];
-  int length = vec.size();
-  for (int i = 0; i < length; i++) {
-    int t = vec[i];
-    if (parent[t] == 0) {
-      parent[t] = v;
-      dfs(t);
-    }
-  }
-}
-
 void solve() {
+  // input
   int i1, i2;
   for (int i = 0; i < n - 1; i++) {
     cin >> i1 >> i2;
     adj[i1].push_back(i2);
     adj[i2].push_back(i1);
   }
+  // bfs
+  queue<int> q;
   parent[1] = 1;
-  for (int k = 1; k <= n; k++) {
-    dfs(k);
+  q.push(1);
+  while (!q.empty()) {
+    int p = q.front();
+    q.pop();
+    vector<int> vec = adj[p];
+    int length = vec.size();
+    for (int i = 0; i < length; i++) {
+      int t = vec[i];
+      if (parent[t] == 0) {
+        parent[t] = p;
+        q.push(t);
+      }
+    }
   }
+  // output
   for (int k = 2; k <= n; k++) {
     cout << parent[k] << "\n";
   }
