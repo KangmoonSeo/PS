eb88ead6fd1741b79e62fc3822daf4c47fa044ae
Kangmoon Seo <100016044+KangmoonSeo@users.noreply.github.com>
2023-05-18 16:59:19 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 728 ms, Memory: 222572 KB -BaekjoonHub

---
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.cc
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+#include <unordered_map>
+using namespace std;
+
+void solve() {
+  string str;
+  cin >> str;
+  // 누적합 전처리
+  unordered_map<char, int> m[200001];
+  int length = str.size();
+
+  m[0][str[0]]++;
+  for (int i = 1; i <= length; i++) {
+    char t = str[i];
+
+    for (int j = 'a'; j <= 'z'; j++) {
+      m[i][j] = m[i - 1][j];
+      if (t == j) m[i][j]++;
+    }
+  }
+
+  int q;
+  cin >> q;
+  char c1;
+  int i2, i3;
+  while (q--) {
+    cin >> c1 >> i2 >> i3;
+    if (i2 == 0)
+      cout << m[i3][c1] << "\n";
+    else
+      cout << m[i3][c1] - m[i2 - 1][c1] << "\n";
+  }
+}
+
+int main() {
+  ios::sync_with_stdio(false), cin.tie(0), cout.tie(0);  // optimization
+
+  solve();
+}
