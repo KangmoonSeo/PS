316275fc8f214c7b2272b13ab007d797caec16eb
Kangmoon Seo <100016044+KangmoonSeo@users.noreply.github.com>
2023-05-25 19:49:05 +0900

    [Gold IV] Title: 사이클 게임, Time: 208 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Gold/20040. 사이클 게임/사이클 게임.cc
+++ 백준/Gold/20040. 사이클 게임/사이클 게임.cc
@@ -2,38 +2,36 @@
 using namespace std;
 
 int n, m;
-int parent[500001];  // saves parent number
-int node_rank[500001] = {};
+int p[500001];       // saves parent number
+int h[500001] = {};  // height
 
 int find_root(int u) {
-  if (u == parent[u]) return u;
-  return find_root(parent[u]);
+  if (p[u] == -1) return u;
+  p[u] = find_root(p[u]);
+  return p[u];
 }
 
-void union_root(int u, int v) {
-  if (node_rank[u] > node_rank[v]) swap(u, v);
-  parent[u] = v;
-  if (node_rank[u] == node_rank[v]) node_rank[v]++;
+bool union_root(int u, int v) {
+  u = find_root(u);
+  v = find_root(v);
+  if (u == v) return true;
+  if (h[u] > h[v]) swap(u, v);
+  p[u] = v;
+  if (h[u] == h[v]) h[v]++;
+  return false;
 }
 
 void solve() {
-  for (int i = 0; i <= n; i++) {
-    parent[i] = i;
-  }
+  fill_n(p, 500001, -1);
   int x1, x2;
   int ans = 0;
   bool isFinish = false;
   for (int i = 1; i <= m; i++) {
     cin >> x1 >> x2;
     if (isFinish) continue;
-
-    int r1 = find_root(x1);
-    int r2 = find_root(x2);
-    if (r1 == r2) {
+    if (union_root(x1, x2)) {
       ans = i;
       isFinish = true;
-    } else {
-      union_root(r1, r2);
     }
   }
   cout << ans << "\n";
