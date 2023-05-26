57f34bdfe4c4f68e78674e3dd3d92a6622ec92c3
Kangmoon Seo <100016044+KangmoonSeo@users.noreply.github.com>
2023-05-26 11:43:03 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 24 ms, Memory: 3696 KB -BaekjoonHub

---
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.cc
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef pair<int, int> pii;
+int n, k;
+int dist[200003];
+
+void solve() {
+  if (n - k > 0) {
+    cout << n - k << "\n";
+    return;
+  }
+  fill_n(dist, 200001, 1e9);
+  priority_queue<pii, vector<pii>, greater<pii> > pq;
+  pq.push(make_pair(0, n));
+  dist[n] = 0;
+  while (!pq.empty()) {
+    int cnt = pq.top().first;
+    int x = pq.top().second;
+    pq.pop();
+    if (dist[x] < cnt) continue;
+
+    int nx;
+    nx = x * 2;
+    if (nx >= 0 && nx <= k * 2 && dist[nx] > cnt) {
+      dist[nx] = cnt;
+      pq.push(make_pair(dist[nx], nx));
+    }
+    nx = x - 1;
+    if (nx >= 0 && nx <= k * 2 && dist[nx] > cnt + 1) {
+      dist[nx] = cnt + 1;
+      pq.push(make_pair(dist[nx], nx));
+    }
+    nx = x + 1;
+    if (nx >= 0 && nx <= k * 2 && dist[nx] > cnt + 1) {
+      dist[nx] = cnt + 1;
+      pq.push(make_pair(dist[nx], nx));
+    }
+  }
+  cout << dist[k] << "\n";
+}
+
+int main() {
+  ios::sync_with_stdio(false), cin.tie(0), cout.tie(0);  // optimization
+
+  cin >> n >> k;
+  solve();
+}
