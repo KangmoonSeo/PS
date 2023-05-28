d636e8c0ea63abe6f0eed6ba342385afb10c4d7b
Kangmoon Seo <100016044+KangmoonSeo@users.noreply.github.com>
2023-05-28 18:01:24 +0900

    [Silver II] Title: N과 M (10), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15664. N과 M （10）/N과 M （10）.cc
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.cc
@@ -1,43 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, m;
-vector<int> arr;
+int n, m, arr[8];
 vector<int> bt;
-set<vector<int> > ans;
 
 void BT(int TTL = 0, int init = 0) {
   if (m == TTL) {
-    ans.insert(bt);
-    return;
-  }
-  for (int i = init; i < n; i++) {
-    bt.push_back(arr[i]);
-    BT(TTL + 1, i + 1);
-    bt.pop_back();
-  }
-}
-void solve() {
-  int input;
-  // input
-  for (int i = 0; i < n; i++) {
-    cin >> input;
-    arr.push_back(input);
-  }
-  // calculate
-  sort(arr.begin(), arr.end());
-  BT();
-  // output
-  for (auto it : ans) {
     for (int i = 0; i < m; i++) {
-      cout << it[i] << " ";
+      cout << bt[i] << " ";
     }
     cout << "\n";
+    return;
+  }
+
+  int pre = 0;
+  for (int i = init; i < n; i++) {
+    if (pre != arr[i]) {
+      pre = arr[i];
+      bt.push_back(arr[i]);
+      BT(TTL + 1, i + 1);
+      bt.pop_back();
+    }
   }
 }
+
 int main() {
   ios::sync_with_stdio(false), cin.tie(0), cout.tie(0);  // optimization
 
   cin >> n >> m;
-  solve();
+  for (int i = 0; i < n; i++) {
+    cin >> arr[i];
+  }
+  sort(arr, arr + n);
+
+  BT();
 }
