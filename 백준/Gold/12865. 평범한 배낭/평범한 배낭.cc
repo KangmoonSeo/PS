dc2f4cfdf70711784c840fc68b757cd07340d4ab
Kangmoon Seo <100016044+KangmoonSeo@users.noreply.github.com>
2023-05-29 15:20:29 +0900

    [Gold V] Title: 평범한 배낭, Time: 48 ms, Memory: 42004 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
@@ -5,25 +5,19 @@ using namespace std;
 int n, k;
 
 void solve() {
-  vector<pair<int, int> > vec(n);
-  int i1, i2;
-  for (int i = 0; i < n; i++) {
-    cin >> i1 >> i2;
-    vec[i] = make_pair(i1, i2);
-  }
-
   vector<vector<int> > P(n + 1, vector<int>(k + 1));
+  int wei, val;
   for (int i = 1; i <= n; i++) {
-    pair<int, int> p = vec[i - 1];
-
-    for (int w = 1; w <= k; w++) {
-      if (w - p.weight >= 0) {
-        P[i][w] = max(P[i - 1][w], p.value + P[i - 1][w - p.weight]);
+    cin >> wei >> val;
+    for (int w = k; w >= 1; w--) {
+      if (w - wei >= 0) {
+        P[i][w] = max(P[i - 1][w], val + P[i - 1][w - wei]);
       } else {
         P[i][w] = P[i - 1][w];
       }
     }
   }
+
   cout << P[n][k] << "\n";
 }
 
