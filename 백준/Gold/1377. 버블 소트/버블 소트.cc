e88582e6e27cdfb24693cf3012b550ae66b7c94b
Kangmoon Seo <100016044+KangmoonSeo@users.noreply.github.com>
2023-06-02 19:15:36 +0900

    [Gold II] Title: 버블 소트, Time: 92 ms, Memory: 8296 KB -BaekjoonHub

---
+++ 백준/Gold/1377. 버블 소트/버블 소트.cc
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n;
+vector<pair<int, int> > arr;
+
+void solve() {
+  sort(arr.begin(), arr.end());
+  int ans = 0;
+  for (int i = 0; i < n; i++) {
+    ans = max(ans, arr[i].second - i);
+  }
+  cout << ans + 1 << "\n";
+}
+
+int main() {
+  ios::sync_with_stdio(false), cin.tie(0), cout.tie(0);  // optimization
+
+  cin >> n;
+  int input;
+  for (int i = 0; i < n; i++) {
+    cin >> input;
+    arr.push_back(make_pair(input, i));
+  }
+  solve();
+}
