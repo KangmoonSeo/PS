738cb830af1f40b33951f3215ce5d73cd5ed462f
Kangmoon Seo <100016044+KangmoonSeo@users.noreply.github.com>
2023-06-04 14:50:39 +0900

    [Gold III] Title: 최소비용 구하기 2, Time: 32 ms, Memory: 4752 KB -BaekjoonHub

--- 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.cc
+++ 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.cc
@@ -5,13 +5,14 @@ typedef pair<int, int> pii;
 int n, m, s, e;
 vector<pii> adj[1001];
 int dist[1001];
-int parent[1001] = {};
+int parent[1001] = {}; // 경로 역추적을 위한 각 정점의 부모 기록
 
 void dijkstra(int start) {
   fill_n(dist, 1001, 1e9);
   priority_queue<pii, vector<pii>, greater<pii> > pq;
   pq.push(make_pair(0, start));
   dist[start] = 0;
+  parent[start] = 0; // parent가 0번을 가르키면 시작점으로 간주
   while (!pq.empty()) {
     int cur_dist = pq.top().first;
     int cur = pq.top().second;
@@ -21,7 +22,7 @@ void dijkstra(int start) {
       int new_dist = p.first + cur_dist;
       if (new_dist < dist[p.second]) {
         dist[p.second] = new_dist;
-        parent[p.second] = cur;
+        parent[p.second] = cur;  // dijkstra에서의 부모 추적 -> 경로
         pq.push(make_pair(new_dist, p.second));
       }
     }
@@ -30,7 +31,7 @@ void dijkstra(int start) {
 vector<int> traceParent() {
   vector<int> arr;
   int cur = e;
-  while (cur != 0) {
+  while (cur != 0) { // parent가 0번을 가르키면 시작점
     arr.push_back(cur);
     cur = parent[cur];
   }
