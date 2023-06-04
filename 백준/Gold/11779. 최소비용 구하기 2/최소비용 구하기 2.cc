030211ca5ab3499b77e36c8ae12b2cab17184a07
Kangmoon Seo <100016044+KangmoonSeo@users.noreply.github.com>
2023-06-04 14:38:03 +0900

    [Gold III] Title: 최소비용 구하기 2, Time: 40 ms, Memory: 4752 KB -BaekjoonHub

---
+++ 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.cc
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef pair<int, int> pii;
+
+int n, m, s, e;
+vector<pii> adj[1001];
+int dist[1001];
+int parent[1001] = {};
+
+void dijkstra(int start) {
+  fill_n(dist, 1001, 1e9);
+  priority_queue<pii, vector<pii>, greater<pii> > pq;
+  pq.push(make_pair(0, start));
+  dist[start] = 0;
+  while (!pq.empty()) {
+    int cur_dist = pq.top().first;
+    int cur = pq.top().second;
+    pq.pop();
+    if (cur_dist > dist[cur]) continue;
+    for (pii p : adj[cur]) {
+      int new_dist = p.first + cur_dist;
+      if (new_dist < dist[p.second]) {
+        dist[p.second] = new_dist;
+        parent[p.second] = cur;
+        pq.push(make_pair(new_dist, p.second));
+      }
+    }
+  }
+}
+vector<int> traceParent() {
+  vector<int> arr;
+  int cur = e;
+  while (cur != 0) {
+    arr.push_back(cur);
+    cur = parent[cur];
+  }
+  return arr;
+}
+
+void solve() {
+  // function acts here
+  dijkstra(s);
+  cout << dist[e] << "\n";
+  vector<int> ans = traceParent();
+  int length = ans.size();
+  cout << length << "\n";
+  for (int i = length - 1; i >= 0; i--) {
+    cout << ans[i] << " ";
+  }
+  cout << "\n";
+}
+
+int main() {
+  ios::sync_with_stdio(false), cin.tie(0), cout.tie(0);  // optimization
+
+  cin >> n >> m;
+  int u, v, w;
+  for (int i = 0; i < m; i++) {
+    cin >> u >> v >> w;
+    adj[u].push_back(make_pair(w, v));
+  }
+  cin >> s >> e;
+
+  solve();
+}
