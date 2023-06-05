46beae0f77ad9024143c09a2ac02ac35aaeacf19
KangmoonSeo <m_oon17@naver.com>
2023-06-05 14:27:52 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 40 ms, Memory: 6916 KB -BaekjoonHub

--- 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.cc
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.cc
@@ -1,12 +1,15 @@
 #include <bits/stdc++.h>
+#define y first
+#define x second
 using namespace std;
 const int dy[4] = {0, 1, 0, -1};
 const int dx[4] = {1, 0, -1, 0};
 
 int n, m;
-int init[2] = {};
 int arr[1001][1001] = {};
 bool visited[1001][1001] = {};
+pair<int, int> init;
+
 void print() {
   for (int j = 1; j <= n; j++) {
     for (int i = 1; i <= m; i++) {
@@ -19,19 +22,20 @@ void print() {
     cout << "\n";
   }
 }
+
 void solve() {
-  //
   queue<pair<int, int> > q;
-  q.push(make_pair(init[0], init[1]));
-  visited[init[0]][init[1]] = true;
-  arr[init[0]][init[1]] = 0;
+  q.push(make_pair(init.y, init.x));
+  visited[init.y][init.x] = true;
+  arr[init.y][init.x] = 0;
+  // bfs
   while (!q.empty()) {
     pair<int, int> p = q.front();
-    int val = arr[p.first][p.second];
+    int val = arr[p.y][p.x];
     q.pop();
     for (int i = 0; i < 4; i++) {
-      int ny = p.first + dy[i];
-      int nx = p.second + dx[i];
+      int ny = p.y + dy[i];
+      int nx = p.x + dx[i];
       if (!visited[ny][nx] && arr[ny][nx] != 0) {
         visited[ny][nx] = true;
         arr[ny][nx] = val + 1;
@@ -39,7 +43,6 @@ void solve() {
       }
     }
   }
-  //
   print();
 }
 
@@ -51,10 +54,10 @@ int main() {
     for (int i = 1; i <= m; i++) {
       cin >> arr[j][i];
       if (arr[j][i] == 0) {
-        visited[j][i] = true;
+        visited[j][i] = true;  // wall
       }
       if (arr[j][i] == 2) {
-        init[0] = j, init[1] = i;
+        init = make_pair(j, i);  // initial point
       }
     }
   }
