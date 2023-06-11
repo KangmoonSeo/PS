99414754ee2c2b3cc4ef094a6ad54ae6dc089b03
Kangmoon Seo <100016044+KangmoonSeo@users.noreply.github.com>
2023-06-11 17:42:35 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 48 ms, Memory: 5236 KB -BaekjoonHub

---
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.cc
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int p[100001];       // saves parent number
+int h[100001] = {};  // height
+int find_root(int u) {
+  if (p[u] == -1) return u;
+  p[u] = find_root(p[u]);
+  return p[u];
+}
+bool union_root(int u, int v) {
+  u = find_root(u);
+  v = find_root(v);
+  if (u == v) return true;
+  if (h[u] > h[v]) swap(u, v);
+  p[u] = v;
+  if (h[u] == h[v]) h[v]++;
+  return false;
+}
+
+typedef pair<int, pair<int, int> > pip;
+priority_queue<pip, vector<pip>, greater<pip> > pq;
+int n, m;
+int ans = 0;
+
+void solve() {
+  // kruskal with disjoint set
+  fill_n(p, 100001, -1);
+
+  for (int i = 0; i < n - 1; i++) {
+    pip e = pq.top();
+    int w = e.first;
+    int u = e.second.first;
+    int v = e.second.second;
+    pq.pop();
+
+    if (union_root(u, v)) {
+      i--;
+      continue;
+    }
+    ans += w;
+  }
+  cout << ans << "\n";
+}
+
+int main() {
+  ios::sync_with_stdio(false), cin.tie(0), cout.tie(0);  // optimization
+
+  cin >> n >> m;
+  int u, v, wei;
+  for (int i = 0; i < m; i++) {
+    cin >> u >> v >> wei;
+    pq.push(make_pair(wei, make_pair(u, v)));
+  }
+
+  solve();
+}
