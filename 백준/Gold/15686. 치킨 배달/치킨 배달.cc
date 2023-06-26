7c050440f9c1f9ba5a1b3a2fb4afb27293da276b
Kangmoon Seo <m_oon17@naver.com>
2023-06-26 16:06:48 +0900

    [Gold V] Title: 치킨 배달, Time: 4 ms, Memory: 2160 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.cc
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.cc
@@ -3,62 +3,41 @@ using namespace std;
 #define y first
 #define x second
 typedef pair<int, int> pii;
-const int dy[4] = {0, 1, 0, -1};
-const int dx[4] = {1, 0, -1, 0};
 
 int n, m;
-int arr[52][52] = {};
-bool visited[52][52] = {};
 vector<pii> store;
 vector<vector<int> > candidates;
-queue<pair<int, pii> > q;  // {dist, {y, x} }
+vector<pii> home;
 
-vector<int> BT;
-void chooseM(int init = 0, int TTL = 0) {
+vector<int> bt;
+void BT(int init = 0, int TTL = 0) {
   if (TTL == m) {
-    candidates.push_back(BT);
+    candidates.push_back(bt);
     return;
   }
   for (int i = init; i < store.size(); i++) {
-    BT.push_back(i);
-    chooseM(i + 1, TTL + 1);
-    BT.pop_back();
+    bt.push_back(i);
+    BT(i + 1, TTL + 1);
+    bt.pop_back();
   }
-}
-
-int bfs() {
-  int ret = 0;
-  while (!q.empty()) {
-    int dist = q.front().first;
-    pii p = q.front().second;
-    q.pop();
-    visited[p.y][p.x] = true;
-    if (arr[p.y][p.x] == 1) {
-      ret += dist;
-    }
-    for (int i = 0; i < 4; i++) {
-      int ny = dy[i] + p.y;
-      int nx = dx[i] + p.x;
-      if (nx < 0 || nx > n || ny < 0 || ny > n) continue;
-      if (!visited[ny][nx]) {
-        visited[ny][nx] = true;
-        q.push({dist + 1, {ny, nx}});
-      }
-    }
-  }
-  return ret;
 }
 
 void solve() {
   int ans = 1e9;
-  chooseM();
+  BT();
   for (int i = 0; i < candidates.size(); i++) {
-    q = queue<pair<int, pii> >();
-    fill_n(visited[0], 52 * 52, false);
-    for (int j = 0; j < candidates[i].size(); j++) {
-      q.push({0, store[candidates[i][j]]});
+    int sum = 0;
+    for (int j = 0; j < home.size(); j++) {
+      pii p_home = home[j];
+      int min_home = 1e9;
+      for (int k = 0; k < candidates[i].size(); k++) {
+        pii p_store = store[candidates[i][k]];
+        min_home = min(min_home,
+                       abs(p_store.y - p_home.y) + abs(p_store.x - p_home.x));
+      }
+      sum += min_home;
     }
-    ans = min(ans, bfs());
+    ans = min(ans, sum);
   }
   cout << ans << "\n";
 }
@@ -67,10 +46,12 @@ int main() {
   ios::sync_with_stdio(false), cin.tie(0), cout.tie(0);  // optimization
 
   cin >> n >> m;
+  int input;
   for (int j = 1; j <= n; j++) {
     for (int i = 1; i <= n; i++) {
-      cin >> arr[j][i];
-      if (arr[j][i] == 2) store.push_back({j, i});
+      cin >> input;
+      if (input == 1) home.push_back({j, i});
+      if (input == 2) store.push_back({j, i});
     }
   }
   solve();
