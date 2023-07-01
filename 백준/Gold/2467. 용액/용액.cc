2e4939da2dd457e79e19e4a102e16251651aa7b0
Kangmoon Seo <m_oon17@naver.com>
2023-07-01 15:25:16 +0900

    [Gold V] Title: 용액, Time: 16 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Gold/2467. 용액/용액.cc
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n;
+int arr[100000];
+int pos = -1;  // negative -> poisitive position
+
+void solve() {
+  // monotonic case
+  if (pos == -1) {  // monotonic negative
+    cout << arr[n - 2] << " " << arr[n - 1] << "\n";
+    return;
+  } else if (pos == 0) {  // monotonic positive
+    cout << arr[0] << " " << arr[1] << "\n";
+    return;
+  }
+
+  // bitonic case; two pointer
+  int l = pos - 1, r = pos;
+  int mem_l = l, mem_r = r, mem = abs(arr[mem_l] + arr[mem_r]);
+  while (l > 0 || r < n - 1) {
+    if (-arr[l] == arr[r]) {
+      cout << arr[l] << " " << arr[r] << "\n";
+      return;
+    }
+    (-arr[l] < arr[r]) ? l-- : r++;  // two pointer
+    if (l < 0 || r > n - 1) break;
+    if (mem > abs(arr[l] + arr[r])) {
+      mem_l = l;
+      mem_r = r;
+      mem = abs(arr[mem_l] + arr[mem_r]);
+    }
+  }
+
+  if (pos - 2 >= 0 && mem > abs(arr[pos - 2] + arr[pos - 1])) {
+    mem_l = pos - 2;
+    mem_r = pos - 1;
+    mem = abs(arr[mem_l] + arr[mem_r]);
+  }
+  if (pos + 1 <= n - 1 && mem > abs(arr[pos] + arr[pos + 1])) {
+    mem_l = pos;
+    mem_r = pos + 1;
+    mem = abs(arr[mem_l] + arr[mem_r]);
+  }
+
+  cout << arr[mem_l] << " " << arr[mem_r] << "\n";
+}
+
+int main() {
+  ios::sync_with_stdio(false), cin.tie(0), cout.tie(0);  // optimization
+
+  cin >> n;
+  for (int i = 0; i < n; i++) {
+    cin >> arr[i];
+    if (pos < 0 && arr[i] > 0) pos = i;
+  }
+
+  solve();
+}
