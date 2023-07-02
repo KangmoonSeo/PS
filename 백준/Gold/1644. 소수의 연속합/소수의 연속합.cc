293240c09aa5c19e6194b404032aeffba9721eb0
Kangmoon Seo <m_oon17@naver.com>
2023-07-02 17:03:04 +0900

    [Gold III] Title: 소수의 연속합, Time: 28 ms, Memory: 9004 KB -BaekjoonHub

--- 백준/Gold/1644. 소수의 연속합/소수의 연속합.cc
+++ 백준/Gold/1644. 소수의 연속합/소수의 연속합.cc
@@ -3,39 +3,30 @@ using namespace std;
 
 int n;
 
-vector<int> getPrime() {
-  vector<int> set;
-  bool isNotPrime[4000001] = {};
-  for (int i = 2; i <= n; i++) {
-    if (isNotPrime[i]) continue;
-    set.push_back(i);
-    for (int j = i; j <= n; j += i) isNotPrime[j] = true;
-  }
-  return set;
-}
-
 void solve() {
   if (n == 1) {
     cout << "0\n";
     return;
   }
-  vector<int> seq = getPrime();
+  vector<int> seq;
+  bool isNotPrime[4000001] = {};
+  seq.push_back(2);
+  for (int i = 3; i <= n; i += 2) {
+    if (isNotPrime[i]) continue;
+    seq.push_back(i);
+    for (int j = i; j <= n; j += i) isNotPrime[j] = true;
+  }
 
   int len = seq.size();
   int l = 0, r = 1;
   int sum = seq[l];  // [l, r)
   int cnt = 0;
-  while (l < r && r <= len) {
-    if (n == sum) {
-      cnt++;
-      sum += seq[r];
-      r++;
-    } else if (n > sum) {
-      sum += seq[r];
-      r++;
+  while (r <= len) {
+    if (n >= sum) {
+      if (n == sum) cnt++;
+      sum += seq[r++];
     } else {
-      sum -= seq[l];
-      l++;
+      sum -= seq[l++];
     }
   }
   cout << cnt << "\n";
