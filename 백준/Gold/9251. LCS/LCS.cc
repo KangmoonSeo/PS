fe1c7704a97daae48ce23079efbed7e8aa5090be
Kangmoon Seo <m_oon17@naver.com>
2023-07-04 16:40:52 +0900

    [Gold V] Title: LCS, Time: 4 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.cc
+++ 백준/Gold/9251. LCS/LCS.cc
@@ -1,20 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-string a;
-string b;
-int dp[1001][1001] = {};
+string a, b;
+int dp[2][1001] = {};
 
 void solve() {
   int a_len = a.size();
   int b_len = b.size();
   for (int i = 1; i <= a_len; i++) {
+    int fl = i % 2;
     for (int j = 1; j <= b_len; j++) {
-      dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
-      if (a[i - 1] == b[j - 1]) dp[i][j] = dp[i - 1][j - 1] + 1;
+      dp[fl][j] = max(dp[1 - fl][j], dp[fl][j - 1]);
+      if (a[i - 1] == b[j - 1]) dp[fl][j] = dp[1 - fl][j - 1] + 1;
     }
   }
-  cout << dp[a_len][b_len] << "\n";
+  cout << dp[a_len % 2][b_len] << "\n";
 }
 
 int main() {
