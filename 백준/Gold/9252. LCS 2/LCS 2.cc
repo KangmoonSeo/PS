da49632ce51aa5d4890816eafbecd2e9f47b1d28
Kangmoon Seo <m_oon17@naver.com>
2023-07-04 17:50:21 +0900

    [Gold IV] Title: LCS 2, Time: 4 ms, Memory: 3980 KB -BaekjoonHub

--- 백준/Gold/9252. LCS 2/LCS 2.cc
+++ 백준/Gold/9252. LCS 2/LCS 2.cc
@@ -2,25 +2,32 @@
 using namespace std;
 
 string a, b;
-string dp[2][1001];
+short dp[1001][1001] = {};
 
+void track(int i, int j) {
+  if (dp[i][j] == 0) return;
+  if (a[i - 1] == b[j - 1]) {
+    track(i - 1, j - 1);
+    cout << a[i - 1];
+  } else {
+    if (dp[i - 1][j] > dp[i][j - 1])
+      track(i - 1, j);
+    else
+      track(i, j - 1);
+  }
+}
 void solve() {
   int a_len = a.size();
   int b_len = b.size();
   for (int i = 1; i <= a_len; i++) {
-    int fl = i % 2;
     for (int j = 1; j <= b_len; j++) {
-      if (a[i - 1] == b[j - 1]) {
-        dp[fl][j] = dp[1 - fl][j - 1];
-        dp[fl][j] += a[i - 1];
-      } else if (dp[1 - fl][j].size() >= dp[fl][j - 1].size()) {
-        dp[fl][j] = dp[1 - fl][j];
-      } else {
-        dp[fl][j] = dp[fl][j - 1];
-      }
+      dp[i][j] = (a[i - 1] == b[j - 1]) ? dp[i - 1][j - 1] + 1
+                                        : max(dp[i - 1][j], dp[i][j - 1]);
     }
   };
-  cout << dp[a_len % 2][b_len].size() << "\n" << dp[a_len % 2][b_len] << "\n";
+  cout << dp[a_len][b_len] << "\n";
+  track(a_len, b_len);
+  cout << "\n";
 }
 
 int main() {
