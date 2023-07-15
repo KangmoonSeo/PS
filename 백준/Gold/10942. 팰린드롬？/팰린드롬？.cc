ff1b75f11576ae7e0a0229c4b2405b9f63a8c47c
Kangmoon Seo <m_oon17@naver.com>
2023-07-15 15:45:10 +0900

    [Gold IV] Title: 팰린드롬?, Time: 196 ms, Memory: 5940 KB -BaekjoonHub

--- 백준/Gold/10942. 팰린드롬？/팰린드롬？.cc
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.cc
@@ -2,7 +2,7 @@
 using namespace std;
 
 int n, m;
-int arr[2001] = {};
+int arr[2001];
 bool dp[2001][2001] = {};
 
 void solve() {
@@ -11,18 +11,10 @@ void solve() {
     cin >> arr[i];
   }
 
-  for (int i = 1; i <= n; i++) {
-    dp[i][i] = true;  // m = 1
-    if (arr[i - 1] == arr[i]) {
-      dp[i - 1][i] = true;  // m = 2
-    }
-  }
-  for (int gap = 2; gap < n; gap++) {  // m > 2
-    for (int i = 1; i <= n - gap; i++) {
-      int j = i + gap;
-      if (arr[i] == arr[j] && dp[i + 1][j - 1]) {
-        dp[i][j] = true;
-      }
+  for (int gap = 0; gap < n; gap++) {
+    for (int s = 1; s <= n - gap; s++) {
+      int e = s + gap;
+      if (arr[s] == arr[e] && (gap < 2 || dp[s + 1][e - 1])) dp[s][e] = true;
     }
   }
 
