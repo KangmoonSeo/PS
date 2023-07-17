e671c574f2675b9a7aa20b7904ea9e24a35a62a1
Kangmoon Seo <m_oon17@naver.com>
2023-07-17 15:45:01 +0900

    [Bronze V] Title: 그대로 출력하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n;
+
+void solve() {
+  string str;
+  getline(cin, str);
+  cout << str << "\n";
+}
+
+int main() {
+  ios::sync_with_stdio(false), cin.tie(0), cout.tie(0); /* FastIO */
+
+  int T = 102;
+  while (T--) {
+    solve();
+  }
+}
