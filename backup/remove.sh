74833ba8b4f51d6278b436422b04ed01ab89a80e
Kangmoon Seo <100016044+KangmoonSeo@users.noreply.github.com>
2023-07-17 16:04:51 +0900

    Refactor: remove.sh 절대경로로 실행하도록 변경

--- backup/remove.sh
+++ backup/remove.sh
@@ -1,14 +1,16 @@
-## default.cpp, .vscode 구성요소, 자기 자신을 제외하고
-## 실행 파일, 폴더 내 7일이 지난 파일을 삭제하는 script
+#!/bin/sh
+## 디렉토리 내 실행 파일과 오래된 코드를 삭제하고 zsh 로그를 정리하는 스크립트
 
 ## config
-__PATH=~/PS/playground
+RELATIVE_DIR=`dirname "$0"`
+cd $RELATIVE_DIR
+SHELL_PATH=`pwd -P`
 
 ## deletion
-find ${__PATH} -mtime +7 -not -path "${__PATH}/remove.sh" -not -path "${__PATH}/default.cpp" -delete
-find ${__PATH} -type f -not -name "*.*" -delete
-find ${__PATH} -name 'tempCodeRunner*' -delete
+find ${SHELL_PATH} -mtime +7 -not -path "${SHELL_PATH}/remove.sh" -not -path "${SHELL_PATH}/default.cpp" -delete
+find ${SHELL_PATH} -type f -not -name "*.*" -delete
+find ${SHELL_PATH} -name 'tempCodeRunner*' -delete
 
 ## .zsh_history optimization
-cat ~/.zsh_history | grep -v 'OneDrive' | grep -v '&&' | grep -v 'curl' | grep -v '{' | grep -v '(' | grep -v 'int' | tail -200 > ~/.zsh_history_temp
+cat ~/.zsh_history | grep -v 'OneDrive' | grep -v '&&' | grep -v 'curl' | grep -v '{' | grep -v '(' | grep -v 'int' | tail -300 > ~/.zsh_history_temp
 mv ~/.zsh_history_temp ~/.zsh_history
