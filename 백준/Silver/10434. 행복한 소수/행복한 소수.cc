66d05129d547e41ffc0feb77e8054ba8974a7a1b
Kangmoon Seo <m_oon17@naver.com>
2023-07-26 16:13:09 +0900

    [Silver II] Title: 행복한 소수, Time: 4 ms, Memory: 2080 KB -BaekjoonHub

--- 백준/Silver/10434. 행복한 소수/행복한 소수.cc
+++ 백준/Silver/10434. 행복한 소수/행복한 소수.cc
@@ -11,28 +11,25 @@ bool getHappy(int val) {
     happy[val] = 0;
   } else if (!visited[val]) {
     visited[val] = true;
-    vector<int> seq;
     int v = val;
-    while (v > 0) {
-      seq.push_back(v % 10);
-      v /= 10;
-    }
     int sum = 0;
-    for (int it : seq) {
-      sum += it * it;
+    while (v > 0) {
+      sum += (v % 10) * (v % 10);
+      v /= 10;
     }
     happy[val] = getHappy(sum);
   }
   return happy[val];
 }
 
-int prime[10001] = {};
+bool prime[10001] = {};
 void getPrime() {
-  prime[2] = 2;
+  prime[2] = true;
   for (int i = 3; i <= 10000; i += 2) {
-    prime[i] = i;
+    prime[i] = true;
   }
   for (int i = 3; i <= 10000; i += 2) {
+    if (!prime[i]) continue;
     for (int j = i * i; j <= 10000; j += i) {
       prime[j] = 0;
     }
@@ -42,7 +39,7 @@ void getPrime() {
 void solve() {
   cin >> n >> m;
   cout << n << " " << m << " ";
-  cout << (happy[m] && prime[m] > 0 ? "YES\n" : "NO\n");
+  cout << (happy[m] && prime[m] ? "YES\n" : "NO\n");
 }
 
 void init() {
