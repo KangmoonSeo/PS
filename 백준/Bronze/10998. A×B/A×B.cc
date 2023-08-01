a7063ae19cc7f38f080a390121c075d3589add6c
Kangmoon Seo <m_oon17@naver.com>
2023-08-01 17:30:17 +0900

    [Bronze V] Title: A×B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int a, b;
+
+
+int main() {
+  ios::sync_with_stdio(false), cin.tie(0), cout.tie(0); /* FastIO */
+
+  cin >> a >> b;
+  cout << a * b << "\n";
+}
