c22ce16270c4106098239fcbe07760ecefa24c98
Kangmoon Seo <100016044+KangmoonSeo@users.noreply.github.com>
2023-08-06 16:57:25 +0900

    Chore: Java PS 환경 준비

---
+++ archive/Main.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static long n;
+
+    public static void solve() {
+        // function works here
+        //
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Long.parseLong(st.nextToken());
+
+        solve();
+    }
+}
