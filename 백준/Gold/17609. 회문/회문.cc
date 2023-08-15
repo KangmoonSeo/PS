293861ffb6f7ec22f2e7c7b9b6c115ff4f507507
Kangmoon Seo <m_oon17@naver.com>
2023-08-15 17:17:28 +0900

    [Gold V] Title: 회문, Time: 16 ms, Memory: 2500 KB -BaekjoonHub

--- 백준/Gold/17609. 회문/회문.cc
+++ 백준/Gold/17609. 회문/회문.cc
@@ -5,26 +5,15 @@ string str;
 
 int check(int l, int r, bool token = false) {
   while (l < r) {
-    char ll = str[l];
-    char rr = str[r];
-    if (ll == rr) {
-      l++;
-      r--;
+    if (str[l] == str[r]) {
+      l++, r--;
     } else {
       if (!token) return -1;
-      if (check(l + 1, r) < 0 && check(l, r - 1) < 0) {
-        return 2;
-      } else {
-        return 1;
-      }
+      if (check(l + 1, r) < 0 && check(l, r - 1) < 0) return 2;
+      return 1;
     }
   }
   return 0;
-}
-
-void solve() {
-  int ans = check(0, str.size() - 1, true);
-  cout << ans << "\n";
 }
 
 int main() {
@@ -35,6 +24,6 @@ int main() {
 
   while (T--) {
     cin >> str;
-    solve();
+    cout << check(0, str.size() - 1, true) << "\n";
   }
 }
