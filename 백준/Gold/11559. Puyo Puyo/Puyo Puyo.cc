3a95bbd649056bfecc89ec20f46b07b0f690dfff
Kangmoon Seo <m_oon17@naver.com>
2023-09-15 16:51:12 +0900

    [Gold IV] Title: Puyo Puyo, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/11559. Puyo Puyo/Puyo Puyo.cc
@@ -0,0 +1,105 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef pair<int, int> pii;
+
+const int H = 12, W = 6;
+char arr[14][8];
+int ans = 0;
+
+const int dy[4] = {0, 1, 0, -1};
+const int dx[4] = {1, 0, -1, 0};
+
+bool bfs(int y, int x) {
+  bool visited[14][8] = {};
+  queue<pii> q;
+  vector<pii> seq;
+
+  q.push({y, x});
+  seq.push_back({y, x});
+  visited[y][x] = true;
+
+  while (!q.empty()) {
+    pii p = q.front();
+    q.pop();
+    for (int i = 0; i < 4; i++) {
+      int ny = dy[i] + p.first;
+      int nx = dx[i] + p.second;
+      if (visited[ny][nx]) continue;
+      visited[ny][nx] = true;
+      if (arr[ny][nx] == '.') continue;
+      if (arr[y][x] == arr[ny][nx]) {
+        q.push({ny, nx});
+        seq.push_back({ny, nx});
+      }
+    }
+  }
+  //
+  if (seq.size() < 4) return false;
+  for (pii p : seq) {
+    arr[p.first][p.second] = '.';
+  }
+  return true;
+}
+
+bool crack() {
+  bool ret = false;
+  for (int j = 1; j <= H; j++) {
+    for (int i = 1; i <= W; i++) {
+      if (arr[j][i] != '.' && bfs(j, i)) {
+        ret = true;
+      }
+    }
+  }
+  return ret;
+}
+void falldown() {
+  for (int i = 1; i <= W; i++) {
+    for (int j = 1; j < H; j++) {
+      if (arr[j][i] == '.') {
+        for (int k = j + 1; k <= H; k++) {
+          if (arr[k][i] != '.') {
+            swap(arr[j][i], arr[k][i]);
+            break;
+          }
+        }
+      }
+    }
+  }
+}
+void print() {
+  for (int j = H; j >= 1; j--) {
+    cout << "|";
+    for (int i = 1; i <= W; i++) {
+      cout << arr[j][i];
+    }
+    cout << "\n";
+  }
+  cout << "\n";
+}
+void solve() {
+  while (crack()) {
+    ans++;
+    // print();
+    falldown();
+    // print();
+  }
+  cout << ans << "\n";
+}
+
+int main() {
+  ios::sync_with_stdio(false), cin.tie(0), cout.tie(0); /* FastIO */
+
+  /**
+   * j=3
+   * j=2
+   * j=1 i=2 i=3
+   */
+  fill_n(arr[0], 14 * 8, '.');
+  for (int j = H; j >= 1; j--) {
+    for (int i = 1; i <= W; i++) {
+      cin >> arr[j][i];
+    }
+  }
+
+  solve();
+}
