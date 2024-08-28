3248ea62b988272c373b3a89142d1d4e3a263d34
Kangmoon Seo <100016044+KangmoonSeo@users.noreply.github.com>
2024-08-28 20:17:45 +0900

    fix: apply changed name

--- archive/run_cleaner.sh
+++ archive/run_cleaner.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 ## 디렉토리 내 실행 파일과 오래된 코드를 삭제하고 zsh 로그를 정리하는 스크립트
 
-## config
+## set parameter
 RELATIVE_DIR=`dirname "$0"`
 cd $RELATIVE_DIR
 SHELL_PATH=`pwd -P`
 
-## deletion
-find ${SHELL_PATH} -mtime +7 -not -path "${SHELL_PATH}/remove.sh" -not -path "${SHELL_PATH}/main.cpp" -not -path "${SHELL_PATH}/Main.java" -delete
+## clean directory
+find ${SHELL_PATH} -mtime +7 -not -path "${SHELL_PATH}/run_cleaner.sh" -not -path "${SHELL_PATH}/main.cpp" -not -path "${SHELL_PATH}/Main.java" -delete
 find ${SHELL_PATH} -type f -not -name "*.*" -delete
 find ${SHELL_PATH} -type f -name "*.class" -delete
 find ${SHELL_PATH} -name 'tempCodeRunner*' -delete
 
-## .zsh_history optimization
+## optimize zsh_history
 cat ~/.zsh_history | grep -v 'OneDrive' | grep -v '&&' | grep -v 'curl' | grep -v '{' | grep -v '(' | grep -v 'int' > ~/.zsh_history_temp
 mv ~/.zsh_history_temp ~/.zsh_history
