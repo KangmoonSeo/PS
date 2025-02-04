bf35984cc96ff50f7314e4a000569d57f32daac1
Kangmoon Seo <m_oon17@naver.com>
2025-02-04 15:34:47 +0900

    [D4] Title: [S/W 문제해결 기본] 9일차 - 중위순회, Time: 11 ms, Memory: 13,552 KB -BaekjoonHub

--- SWEA/D4/1231. ［S／W 문제해결 기본］ 9일차 － 중위순회/［S／W 문제해결 기본］ 9일차 － 중위순회.cpp
+++ SWEA/D4/1231. ［S／W 문제해결 기본］ 9일차 － 중위순회/［S／W 문제해결 기본］ 9일차 － 중위순회.cpp
@@ -1,64 +1,140 @@
-#include <bits/stdc++.h>
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 기본 제공코드는 임의 수정해도 관계 없습니다. 단, 입출력 포맷 주의
+// 아래 표준 입출력 예제 필요시 참고하세요.
+// 표준 입력 예제
+// int a;
+// float b, c;
+// double d, e, f;
+// char g;
+// char var[256];
+// long long AB;
+// cin >> a;                            // int 변수 1개 입력받는 예제
+// cin >> b >> c;                       // float 변수 2개 입력받는 예제
+// cin >> d >> e >> f;                  // double 변수 3개 입력받는 예제
+// cin >> g;                            // char 변수 1개 입력받는 예제
+// cin >> var;                          // 문자열 1개 입력받는 예제
+// cin >> AB;                           // long long 변수 1개 입력받는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 표준 출력 예제
+// int a = 0;
+// float b = 1.0, c = 2.0;
+// double d = 3.0, e = 0.0; f = 1.0;
+// char g = 'b';
+// char var[256] = "ABCDEFG";
+// long long AB = 12345678901234567L;
+// cout << a;                           // int 변수 1개 출력하는 예제
+// cout << b << " " << c;               // float 변수 2개 출력하는 예제
+// cout << d << " " << e << " " << f;   // double 변수 3개 출력하는 예제
+// cout << g;                           // char 변수 1개 출력하는 예제
+// cout << var;                         // 문자열 1개 출력하는 예제
+// cout << AB;                          // long long 변수 1개 출력하는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+#include <iostream>
+#include <vector>
 using namespace std;
-typedef pair<int, int> pii;
 
-int n;
-vector<pair<char, pii>> vec(101);  // {value, {l_idx, r_idx} }
-string ans;
+struct Node {
+  string val;
+  int l_index;
+  int r_index;
+};
 
-void recur(int idx) {
-  auto p = vec[idx];
-  if (p.second.first > 0) recur(p.second.first);
-  ans += p.first;
-  if (p.second.second > 0) recur(p.second.second);
+vector<Node*> nodes;
+
+void print() {
+  return;
+  cout << nodes.size() << "| ";
+  for (int i = 0; i < nodes.size(); i++) {
+    auto target = nodes[i];
+    cout << "(i=" << i << ",val=" << target->val << ",l=" << target->l_index
+         << ",r=" << target->r_index << ")";
+  }
+  cout << "\n";
 }
-
-vector<string> split(string str, char delimiter) {
-  vector<string> ret;
-  string buf = "";
-
-  for (char c : str) {
-    if (c == delimiter) {
-      ret.push_back(buf);
+Node* parse_node_from_string(string input) {
+  // parse
+  vector<string> vec;
+  string buf;
+  for (char c : input) {
+    if (c == ' ') {
+      vec.push_back(buf);
       buf = "";
     } else {
       buf += c;
     }
   }
-  ret.push_back(buf);
-  return ret;
+  if (buf != "") {
+    vec.push_back(buf);
+  }
+  // build node
+  Node* node = new Node();
+  node->val = vec[1][0];
+  node->r_index = ((vec.size() > 3) ? stoi(vec[3]) : -1);
+  node->l_index = ((vec.size() > 2) ? stoi(vec[2]) : -1);
+  return node;
+}
+
+string traversal(int idx) {
+  int l = nodes[idx]->l_index;
+  int r = nodes[idx]->r_index;
+  string val = nodes[idx]->val;
+
+  if (l > 0 && r > 0) {
+    return traversal(l) + val + traversal(r);
+  } else if (l > 0 && r < 0) {
+    return traversal(l) + val;
+  } else {
+    return val;
+  }
 }
 
 string solve() {
-  ans = "";
-
+  int n;
   string input;
+
+  // align index
+  nodes = vector<Node*>();
+  nodes.push_back(parse_node_from_string("0 X 0 0"));
+
   getline(cin, input);
   n = stoi(input);
 
-  for (int i = 0; i < n; i++) {
+  // get line
+  for (int i = 1; i <= n; i++) {
     getline(cin, input);
 
-    char val;
-    int idx, ll = 0, rr = 0;
-    auto splitters = split(input, ' ');
-    idx = stoi(splitters[0]);
-    val = splitters[1][0];
-    if (splitters.size() > 2) ll = stoi(splitters[2]);
-    if (splitters.size() > 3) rr = stoi(splitters[3]);
-    vec[idx] = {val, {ll, rr}};
+    Node* new_node = parse_node_from_string(input);
+    nodes.push_back(new_node);
   }
 
-  recur(1);
-
-  return ans;
+  // in-order 순회
+  string ret = traversal(1);
+  return ret;
 }
 
-int main() {
-  ios::sync_with_stdio(false), cin.tie(0), cout.tie(0); /* FastIO */
-
-  for (int test_case = 1; test_case <= 10; test_case++) {
-    string sol = solve();
-    cout << "#" << test_case << " " << sol << "\n";
+int main(int argc, char** argv) {
+  int test_case;
+  int T;
+  /*
+     아래의 freopen 함수는 input.txt 를 read only 형식으로 연 후,
+     앞으로 표준 입력(키보드) 대신 input.txt 파일로부터 읽어오겠다는 의미의
+     코드입니다.
+     //여러분이 작성한 코드를 테스트 할 때, 편의를 위해서 input.txt에 입력을
+     저장한 후, freopen 함수를 이용하면 이후 cin 을 수행할 때 표준 입력 대신
+     파일로부터 입력을 받아올 수 있습니다. 따라서 테스트를 수행할 때에는 아래
+     주석을 지우고 이 함수를 사용하셔도 좋습니다. freopen 함수를 사용하기
+     위해서는 #include <cstdio>, 혹은 #include <stdio.h> 가 필요합니다. 단,
+     채점을 위해 코드를 제출하실 때에는 반드시 freopen 함수를 지우거나 주석 처리
+     하셔야 합니다.
+  */
+  // freopen("input.txt", "r", stdin);
+  /*
+     여러 개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+  */
+  for (test_case = 1; test_case <= 10; ++test_case) {
+    string ans = solve();
+    cout << "#" << test_case << " " << ans << "\n";
   }
+  return 0;  // 정상종료시 반드시 0을 리턴해야합니다.
 }
