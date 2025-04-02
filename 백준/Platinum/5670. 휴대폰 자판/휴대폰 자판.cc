375dbc3c062cb2644fcb57219b6f80bea0bd7552
Kangmoon Seo <m_oon17@naver.com>
2025-04-02 18:44:16 +0900

    [Platinum IV] Title: 휴대폰 자판, Time: 268 ms, Memory: 179092 KB -BaekjoonHub

--- 백준/Platinum/5670. 휴대폰 자판/휴대폰 자판.cc
+++ 백준/Platinum/5670. 휴대폰 자판/휴대폰 자판.cc
@@ -1,20 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 int n;
 
 struct Node {
-  Node* children['z' - 'a' + 1];
+  Node* children['z' - 'a' + 1] = {nullptr};
   int children_size = 0;
   bool isEnd = false;
 };
+
+const int MAX_NODES = 800001;
+Node node_pool[MAX_NODES];
+int node_cnt = 0;
+
+Node* get_node() {
+  // 새로운 노드 할당
+  Node* new_node = &node_pool[node_cnt++];
+
+  // 초기화
+  for (int i = 0; i < ('z' - 'a' + 1); i++) {
+    new_node->children[i] = nullptr;
+  }
+  new_node->children_size = 0;
+  new_node->isEnd = false;
+
+  return new_node;
+}
 
 void register_word(Node* root, string s) {
   Node* current_node = root;
   for (auto c : s) {
     int idx = c - 'a';
     if (current_node->children[idx] == nullptr) {
-      current_node->children[idx] = new Node();
+      current_node->children[idx] = get_node();
       current_node->children_size += 1;
     }
     current_node = current_node->children[idx];
@@ -23,17 +40,17 @@ void register_word(Node* root, string s) {
 }
 
 int validate_word(Node* root, string word) {
-  //
   int cnt = 0;
-
   Node* current_node = root;
+
   // i=0; initially typed
   current_node = current_node->children[word[0] - 'a'];
   cnt += 1;
+
   // i>0
   for (int i = 1; i < word.size(); i++) {
     int idx = word[i] - 'a';
-    // 2. try autocomplete
+    // try autocomplete
     if (current_node->children_size == 1 && !current_node->isEnd) {
       // autocompeleted
       current_node = current_node->children[idx];
@@ -46,18 +63,14 @@ int validate_word(Node* root, string word) {
   return cnt;
 }
 
-void delete_trie(Node* node) {
-  if (node == nullptr) return;
-  for (int i = 0; i < ('z' - 'a' + 1); i++) {
-    delete_trie(node->children[i]);  // 자식 노드 재귀적으로 삭제
-  }
-  delete node;  // 부모 노드 삭제
-}
 double solve() {
-  Node* root = new Node();
-  vector<string> words;
+  // 노드 카운터 초기화
+  node_cnt = 0;
 
+  Node* root = get_node();
+  vector<string> words;
   string input;
+
   for (int i = 0; i < n; i++) {
     cin >> input;
     words.push_back(input);
@@ -68,14 +81,13 @@ double solve() {
   for (auto word : words) {
     cnt += validate_word(root, word);
   }
-  delete_trie(root);
+
   double ans = (double)cnt / words.size();
   return ans;
 }
 
 int main() {
   ios::sync_with_stdio(false), cin.tie(0), cout.tie(0); /* FastIO */
-
   while (cin >> n) {
     double ans = solve();
     cout << fixed << setprecision(2) << ans << "\n";
