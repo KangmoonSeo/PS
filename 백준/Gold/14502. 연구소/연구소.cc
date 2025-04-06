451fd4f8e6dad3e4372f8a71123a951ce0d62922
Kangmoon Seo <m_oon17@naver.com>
2025-04-06 20:34:11 +0900

    [Gold IV] Title: 연구소, Time: 44 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.cc
+++ 백준/Gold/14502. 연구소/연구소.cc
@@ -1,28 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define y first
-#define x second
-#define WALL 1
 typedef pair<int, int> pii;
 const int dy[4] = {1, 0, -1, 0};
 const int dx[4] = {0, 1, 0, -1};
 
 int n, m;
-vector<pii> zeros;
-queue<pii> contams;
-vector<vector<int> > arr(10, vector<int>(10, WALL));  // initial input
-vector<vector<int> > sp;                              // space for simulation
+queue<pii> contamPoint;
+vector<pii> zeroPoint;
+vector<vector<int> > arr(10, vector<int>(10, 1));  // initial space
+vector<vector<int> > space;
 
 void simulate() {
-  queue<pii> q = contams;
+  queue<pii> q = contamPoint;
   while (!q.empty()) {
     pii p = q.front();
     q.pop();
     for (int i = 0; i < 4; i++) {
-      int ny = p.y + dy[i];
-      int nx = p.x + dx[i];
-      if (sp[ny][nx] == 0) {  // no 'visited' array
-        sp[ny][nx] = 2;
+      int ny = p.first + dy[i];
+      int nx = p.second + dx[i];
+      if (space[ny][nx] == 0) {
+        space[ny][nx] = 2;
         q.push({ny, nx});
       }
     }
@@ -32,21 +29,21 @@ int countZero() {
   int ret = 0;
   for (int j = 1; j <= n; j++) {
     for (int i = 1; i <= m; i++) {
-      if (sp[j][i] == 0) ret++;
+      if (space[j][i] == 0) ret++;
     }
   }
   return ret;
 }
 void solve() {
   int ans = 0;
-  int length = zeros.size();
+  int length = zeroPoint.size();
   for (int i = 0; i < length; i++) {
     for (int j = i + 1; j < length; j++) {
       for (int k = j + 1; k < length; k++) {
-        sp = arr;
-        sp[zeros[i].y][zeros[i].x] = WALL;
-        sp[zeros[j].y][zeros[j].x] = WALL;
-        sp[zeros[k].y][zeros[k].x] = WALL;
+        space = arr;
+        space[zeroPoint[i].first][zeroPoint[i].second] = 1;
+        space[zeroPoint[j].first][zeroPoint[j].second] = 1;
+        space[zeroPoint[k].first][zeroPoint[k].second] = 1;
         simulate();
         ans = max(ans, countZero());
       }
@@ -62,8 +59,8 @@ int main() {
   for (int j = 1; j <= n; j++) {
     for (int i = 1; i <= m; i++) {
       cin >> arr[j][i];
-      if (arr[j][i] == 0) zeros.push_back({j, i});  // for brute force
-      if (arr[j][i] == 2) contams.push({j, i});     // for bfs
+      if (arr[j][i] == 0) zeroPoint.push_back({j, i});
+      if (arr[j][i] == 2) contamPoint.push({j, i});
     }
   }
   solve();
@@ -71,5 +68,5 @@ int main() {
 
 /*
  * 23.06.23. 16:52 start
- * 23.06.23. 17:29 end
+ * 23.06.23. 17:20 end
  */
