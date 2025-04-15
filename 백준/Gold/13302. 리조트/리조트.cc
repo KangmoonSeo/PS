dbd55577fadae8ab19d7af83ce14fd9c176703d8
Kangmoon Seo <m_oon17@naver.com>
2025-04-15 15:52:56 +0900

    [Gold IV] Title: 리조트, Time: 0 ms, Memory: 2024 KB, Score: 28 point -BaekjoonHub

---
+++ 백준/Gold/13302. 리조트/리조트.cc
@@ -0,0 +1,84 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define MAX_COUPON 10
+int n, m;
+int not_visited[101];
+int dp[101][MAX_COUPON];
+
+/**
+ * n: 수영이의 여름방학의 일수
+ * m: 수영이가 리조트에 갈 수 없는 날의 수
+ *
+ * arr: 수영이가 리조트에 갈 수 없는 날짜, arr번째
+ *
+ * @brief 가격표
+ *
+ *
+ * @returns
+ * (int): 주어진 입력에서 제시된 날을 제외한 나머지 날 모두
+ *     리조트에 입장하기 위해서 지불해야 하는 비용의 최솟값
+ */
+int solve() {
+  // function works here
+  //
+  int input;
+  for (int i = 0; i < m; i++) {
+    cin >> input;
+    not_visited[input] = true;
+  }
+
+  fill_n(dp[0], 101 * MAX_COUPON, 1e9);
+
+  // i=0
+  dp[0][0] = 0;
+
+  // i>0
+  for (int i = 1; i <= n; i++) {
+    // 하루 이용권 구매
+    for (int j = 0; j < MAX_COUPON; j++) {
+      if (not_visited[i]) {
+        dp[i][j] = dp[i - 1][j] + 0;
+      } else {
+        dp[i][j] = dp[i - 1][j] + 10000;
+      }
+    }
+
+    // 연속 3일권 구매
+    if (i >= 3) {
+      for (int j = 1; j < MAX_COUPON; j++) {
+        dp[i][j] = min(dp[i][j], dp[i - 3][j - 1] + 25000);
+      }
+    }
+
+    // 연속 5일권 구매
+    if (i >= 5) {
+      for (int j = 2; j < MAX_COUPON; j++) {
+        dp[i][j] = min(dp[i][j], dp[i - 5][j - 2] + 37000);
+      }
+    }
+
+    // 쿠폰 사용
+    if (!not_visited[i]) {
+      for (int j = 3; j < MAX_COUPON; j++) {
+        if (dp[i - 1][j] < 1e9) {
+          dp[i][j - 3] = min(dp[i][j - 3], dp[i - 1][j]);
+        }
+      }
+    }
+  }
+  int ans = 1e9;
+
+  for (int j = 0; j < MAX_COUPON; j++) {
+    ans = min(ans, dp[n][j]);
+  }
+  return ans;
+}
+
+int main() {
+  ios::sync_with_stdio(false), cin.tie(0), cout.tie(0); /* FastIO */
+
+  cin >> n >> m;
+  auto ans = solve();
+  cout << ans << "\n";
+}
