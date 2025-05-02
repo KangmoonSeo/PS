43f4161b854065865af995ef22862c392e98948d
Kangmoon Seo <m_oon17@naver.com>
2025-05-02 15:27:35 +0900

    [level 3] Title: 있었는데요 없었습니다, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
+++ 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
@@ -1,5 +1,6 @@
 -- 코드를 입력하세요
-SELECT i.animal_id, i.name from animal_ins as i, animal_outs as o
+SELECT i.animal_id as 'ANIMAL_ID', i.name as 'NAME'
+from animal_ins as i, animal_outs as o
 where i.animal_id = o.animal_id
 and i.datetime > o.datetime
 order by i.datetime asc
