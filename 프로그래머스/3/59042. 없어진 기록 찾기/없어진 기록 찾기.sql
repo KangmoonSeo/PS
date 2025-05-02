bf75fb76079631254e7b4d6ea6fac77497016547
Kangmoon Seo <m_oon17@naver.com>
2025-05-02 14:59:35 +0900

    [level 3] Title: 없어진 기록 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
+++ 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
@@ -1,5 +1,7 @@
-select animal_id, name 
-from animal_outs as o
+-- 코드를 입력하세요
+SELECT animal_id, name 
+from animal_outs
 where animal_id not in (
-    select animal_id from animal_ins
-)
+    select animal_id
+    from animal_ins
+    )
